8ddcd497ee4cd7bced8511f950fef613112988fc
Marouane TAKI EDINE <maroinetakiedine@gmail.com>
2024-01-28 10:18:00 +0100

    Add the new link of the front to bypass CORS 2

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.set('debug', true);
 
  app.use(cors({
    credentials:true,
-   origin:['http://localhost:4200',"https://app-miage-assignments.netlify.app/"]
+   origin:['http://localhost:4200',"https://app-miage-assignments.netlify.app"]
  }))
 
 
